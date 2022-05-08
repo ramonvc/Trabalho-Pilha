f335f08ed14b1c0d4c5c432ae8f6d5c108b5b7dc
Ramon Victor Cardoso <ramonvc19@gmail.com>
2022-05-07 22:21:45 -0300

    Update Story.java

--- Story.java
+++ Story.java
@@ -1,12 +1,16 @@
 public class Story {
     private int top;
+    int cont;
     private String contact[];
-    private String oldVersion; // Guarda a penúltima versão
+    private String oldVersion[]; // Guarda a penúltima versão
+    boolean y;
 
     public Story(int size) {
         this.contact = new String[size];
-        this.oldVersion = new String();
+        this.oldVersion = new String[size];
         this.top = 0;
+        this.cont = 0;
+        this.y = false;
     }
 
     public boolean isEmpty() {
@@ -19,12 +23,18 @@ public class Story {
 
     public void CtrlY() {
         // Anula a última restauração
-        this.push(oldVersion);
+        if(y && this.oldVersion[cont - 1] != null) {
+            this.push(this.oldVersion[cont - 1]);
+            cont -= 1;
+            this.y = true;
+        }
     }
 
     public void CtrlZ() {
         // Restaura a última versão do contato
-        this.oldVersion = this.pop();
+        this.oldVersion[cont] = this.pop();
+        this.y = true;
+        this.cont += 1;
     }
 
     public void push(String cont) throws ArrayIndexOutOfBoundsException {
@@ -34,6 +44,7 @@ public class Story {
 
         this.contact[top] = cont.toString();
         this.top += 1;
+        this.y = false;
     }
 
     public String pop() throws ArrayIndexOutOfBoundsException {
@@ -44,6 +55,7 @@ public class Story {
         this.top -= 1;
         String con = this.contact[this.top];
         this.contact[top] = null;
+        this.y = false;
         return con;
     }
 
